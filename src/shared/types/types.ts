4f57a21b31b0a02baed865bd2e970300f2d66d6a
kostili-tec <weh4teyou@gmail.com>
2023-10-03 16:05:04 +0300

    feat: add response types

---
+++ src/shared/types/types.ts
@@ -0,0 +1,30 @@
+export interface ResponseCommentData {
+    id: number;
+    created: string | Date;
+    text: string;
+    author: number;
+    parent: null | number;
+    likes: number;
+}
+
+export interface ResponseAuthor {
+    id: number;
+    name: string;
+    avatar: string;
+}
+
+interface ResponseCommentPagination {
+    page: number;
+    size: number;
+    total_pages: number;
+}
+
+export type ResponseComment = {
+    data: ResponseCommentData[];
+    pagination: ResponseCommentPagination;
+};
+
+export enum ApiUrls {
+    AUTHORS = "/api/authors",
+    COMMENTS = "/api/comments",
+}
